f3c4735f66e5df30ee6e40031982e340928b7faf
SamuelWiqvist <swiqvist@gmail.com>
2015-09-17 16:48:26 +0200

    added methods

--- spline.py
+++ spline.py
@@ -1,6 +1,24 @@
+import numpy as np
+import scipy.linalg as sl
+
 class Spline:
     def __init__(self, grid, control_points):
         self.grid = grid;
         self.control_points = control_points
+    
+"""
+    def __call__(self, u):
+        
+    def _alpha_(u):
+        
+    def _find_interval_(self, u):
+    
+    def _find_controlPoint_(self, u):
+        
+    def _blossom(self, u):
+        
+    def plot(self, points):
+"""
+
 
 
