1a6139782b1b474ed89205dc3bbbc81cc97399d7
SamuelWiqvist <swiqvist@gmail.com>
2015-09-18 16:16:11 +0200

    some changes

--- runScript.py
+++ runScript.py
@@ -8,24 +8,6 @@ control_points = np.array([[3,4.5,5,6,7,8,8.6],[4,5,6,8,4,6,7.2]])# L = K - 2 = 
 #%%
 s = Spline(grid, control_points) # initialize the spline 
 #%%
-u = 3.5
-s(u) # run __call__   
-#%%
-points = np.linspace(0,10,100).tolist()
-plot(s,points) #plot spline
-#%%
-u = np.linspace(0,10,10).tolist() #knot sequence
-j = 5 # index
-eval_basis(u, j)  #evaluate j:th B-spline basis
-
-
-#%%
-i = 2
-col1 = [control_points[0][i-2], control_points[0][i-1], control_points[0][i], control_points[0][i+1]]
-col2 = [control_points[1][i-2], control_points[1][i-1], control_points[1][i], control_points[1][i+1]]
-#%%
-t = np.array([col1, col2])
-#%%
 s._find_interval(3.5)
 #%%
 t = s._find_controlPoints(3.5)
@@ -33,3 +15,5 @@ t = s._find_controlPoints(3.5)
 s._alpha(3.5)
 #%%
 s._blossom(3.5)
+#%%
+t = eval_basis(s.grid, 3)
