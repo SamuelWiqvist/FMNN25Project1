137f2707075da1e304c333967449323e27879d24
SamuelWiqvist <swiqvist@gmail.com>
2015-09-18 14:06:14 +0200

    m

--- runScript.py
+++ runScript.py
@@ -2,8 +2,9 @@ import numpy as np
 import scipy.linalg as sl
 
 #%%
-grid = np.linspace(0,10,10).tolist() # K = 9
-control_points = [(3.2,4),(4.5,5),(5,6),(6,8),(7,4), (8,6),(8.6,7)] # L = K - 2 = 7
+grid = np.linspace(0,10,10) # K = 9
+control_points = np.array([[3,4.5,5,6,7,8,8.6],[4,5,6,8,4,6,7.2]])# L = K - 2 = 7
+#%%
 s = Spline(grid, control_points) # initialize the spline 
 #%%
 u = 3.5
@@ -17,3 +18,10 @@ j = 5 # index
 eval_basis(u, j)  #evaluate j:th B-spline basis
 
 
+#%%
+i = 2
+col1 = [control_points[0][i-2], control_points[0][i-1], control_points[0][i], control_points[0][i+1]]
+col2 = [control_points[1][i-2], control_points[1][i-1], control_points[1][i], control_points[1][i+1]]
+#%%
+t = np.array([col1, col2])
+#%%
--- spline.py
+++ spline.py
@@ -5,20 +5,28 @@ class Spline:
     def __init__(self, grid, control_points):
         self.grid = grid;
         self.control_points = control_points
-    
-"""
+    def _find_controlPoints_(self, u, i):
+        # returns an array with four first control points
+        cp = self.control_points
+        col1 = [cp[0][i-2], cp[0][i-1], cp[0][i], cp[0][i+1]]
+        col2 = [cp[1][i-2], cp[1][i-1], cp[1][i], cp[1][i+1]] 
+        cp_first = np.array([col1, col2])
+        return cp_first
+'''
     def __call__(self, u):
-        
+'''
     def _alpha_(u):
         
+'''     
     def _find_interval_(self, u):
-    
-    def _find_controlPoint_(self, u):
+'''                     
         
+'''            
     def _blossom(self, u):
         
     def plot(s, points):
-"""
+'''
+
 def eval_basis(u, j): # just something I came up with, I dont know if it is correct / Samuel
     control_points = [(0,0)]
     for i in range(1, len(u)-3):
@@ -27,6 +35,4 @@ def eval_basis(u, j): # just something I came up with, I dont know if it is corr
     s_temp = Spline(u,control_points)
     points = np.linspace(min(u),max(u),100).tolist()     
     plot(s_temp, points)
-
-
 
